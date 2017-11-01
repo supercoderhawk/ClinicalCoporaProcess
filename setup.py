326dbf583e295f08e3b2ae65c273a1291147b6a5
supercoderhawk <supercoderhawk@gmail.com>
2017-11-01 22:08:07 +0800

    add dependencies lib

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ from setuptools import setup
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
+    extra = ['pandas','lxml']
+    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + extra+ sphinx,
           use_pyscaffold=True)
 
 
